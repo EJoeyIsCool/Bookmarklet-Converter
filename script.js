1862fb9aeb9156e9507ea36ffcfec7c1def9718e
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2024-03-30 16:58:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.innerHTML = document.body.innerHTML.replace(/%([0-9A-Fa-f]{2})/g, function(match, p1) {
+document.getElementById('convertBtn').addEventListener('click', function() {
+  const scriptInput = document.getElementById('scriptInput').value;
+  const convertedScript = scriptInput.replace(/%([0-9A-Fa-f]{2})/g, function(match, p1) {
     return String.fromCharCode(parseInt(p1, 16));
   });
+  document.getElementById('convertedScript').textContent = convertedScript;
 });
